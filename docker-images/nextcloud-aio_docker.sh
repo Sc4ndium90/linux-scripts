1934e6b3862151057965c3b96a73eff6e4ba51f4
Scandium90 <38154120+Sc4ndium90@users.noreply.github.com>
2024-04-23 17:08:45 +0200

    Update nextcloud-aio_docker.sh

--- docker-images/nextcloud-aio_docker.sh
+++ docker-images/nextcloud-aio_docker.sh
@@ -21,16 +21,16 @@ else
     exit 1
 fi
 
-sudo docker run -it \
+sudo docker run \
+--init \
+--sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
 --restart always \
--p 80:80 \
--p 8080:8080 \
--p 8443:8443 \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--publish 8080:8080 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
 -e NEXTCLOUD_DATADIR="$dirname" \
 -e SKIP_DOMAIN_VALIDATION=true \
--e APACHE_PORT=11000 \
--e APACHE_IP_BINDING=0.0.0.0 \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest
