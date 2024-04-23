7ce0c90e84c94fb9c2e0d9a3d5789d28d2178df4
Scandium90 <38154120+Sc4ndium90@users.noreply.github.com>
2024-04-23 11:04:04 +0200

    Update docker-install_debian.sh

--- docker-install_debian.sh
+++ docker-install_debian.sh
@@ -2,9 +2,9 @@
 
 echo "Installing Docker.."
 
-apt-get update && apt-get upgrade -y
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
+apt update && apt upgrade -y
+apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -14,7 +14,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 echo "Specify a user that can use docker without root privileges:"
