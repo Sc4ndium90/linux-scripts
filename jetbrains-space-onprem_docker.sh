438f68dc5d02acc6234fd53965c82061252ad7ad
Scandium90 <38154120+Sc4ndium90@users.noreply.github.com>
2024-04-23 11:27:34 +0200

    Update jetbrains-space-onprem_docker.sh

--- jetbrains-space-onprem_docker.sh
+++ jetbrains-space-onprem_docker.sh
@@ -23,7 +23,7 @@ mkdir -p space-on-premises && cd space-on-premises
 export SPACE_RELEASE_NAME="2024.1.0"
 curl -O "https://assets.on-premises.service.jetbrains.space/${SPACE_RELEASE_NAME}/docker-compose.yml"
 
-docker-compose -p space-on-premises up -d
+docker compose -p space-on-premises up -d
 docker ps
 
 echo "${GREEN}Installation done${NC}"
