2fb88f04ae6a16f13de899a9af28ae6317cf5e26
Scandium90 <38154120+Sc4ndium90@users.noreply.github.com>
2024-04-23 11:24:17 +0200

    Create jetbrains-space-onprem_docker.sh

---
+++ jetbrains-space-onprem_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo "Installing Jetbrains Space On-Premise on Docker.."
+
+echo "Checking if Docker and Docker Compose plugin are installed"
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    echo "${GREEN}> Docker is installed.${NC}"
+else
+    echo "${RED}> Docker is not installed.${NC}"
+    exit 1
+fi
+
+mkdir -p space-on-premises && cd space-on-premises
+
+export SPACE_RELEASE_NAME="2024.1.0"
+curl -O "https://assets.on-premises.service.jetbrains.space/${SPACE_RELEASE_NAME}/docker-compose.yml"
+
+docker-compose -p space-on-premises up -d
+docker ps
+
+echo "${GREEN}Installation done${NC}"
