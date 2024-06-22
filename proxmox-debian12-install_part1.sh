1d96206378fc97025cbd731621c5cc8ae51a4c9b
Sc4ndium90 <38154120+Sc4ndium90@users.noreply.github.com>
2024-06-22 17:19:26 +0200

    Create proxmox-debian12-install_part1.sh

---
+++ proxmox-debian12-install_part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt install wget
+
+echo "pve" >> /ect/hostname
+echo "<IP> pve" >> /etc/hosts
+
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
+sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
+
+apt update && apt full-upgrade
+sudo dpkg --purge --force-all firmware-ath9k-htc
+sudo apt --fix-broken install
+
+apt install proxmox-default-kernel
+systemctl reboot
